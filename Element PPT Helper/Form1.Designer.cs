69a00eb1325e1dd9c6c13ee3050dcef83469948d
Fe3O4-Git <Fe3O4_Live@Outlook.com>
2022-03-13 10:15:44 +0800

    dev ppt helper

--- Element PPT Helper/Form1.Designer.cs
+++ Element PPT Helper/Form1.Designer.cs
@@ -31,11 +31,13 @@
             this.SwitchToPointerButton = new System.Windows.Forms.Button();
             this.SwitchToPenButton = new System.Windows.Forms.Button();
             this.SwitchToEraserButton = new System.Windows.Forms.Button();
+            this.NextPageButton = new System.Windows.Forms.Button();
+            this.PreviousPageButton = new System.Windows.Forms.Button();
+            this.ExitButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // SwitchToPointerButton
             // 
-            this.SwitchToPointerButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.SwitchToPointerButton.Location = new System.Drawing.Point(0, 0);
             this.SwitchToPointerButton.Name = "SwitchToPointerButton";
             this.SwitchToPointerButton.Size = new System.Drawing.Size(36, 36);
@@ -46,8 +48,6 @@
             // 
             // SwitchToPenButton
             // 
-            this.SwitchToPenButton.BackColor = System.Drawing.Color.Transparent;
-            this.SwitchToPenButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.SwitchToPenButton.Location = new System.Drawing.Point(36, 0);
             this.SwitchToPenButton.Name = "SwitchToPenButton";
             this.SwitchToPenButton.Size = new System.Drawing.Size(36, 36);
@@ -58,8 +58,6 @@
             // 
             // SwitchToEraserButton
             // 
-            this.SwitchToEraserButton.BackColor = System.Drawing.Color.Transparent;
-            this.SwitchToEraserButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.SwitchToEraserButton.Location = new System.Drawing.Point(72, 0);
             this.SwitchToEraserButton.Name = "SwitchToEraserButton";
             this.SwitchToEraserButton.Size = new System.Drawing.Size(36, 36);
@@ -68,19 +66,54 @@
             this.SwitchToEraserButton.UseVisualStyleBackColor = false;
             this.SwitchToEraserButton.Click += new System.EventHandler(this.SwitchToEraserButton_Click);
             // 
+            // NextPageButton
+            // 
+            this.NextPageButton.Location = new System.Drawing.Point(324, 0);
+            this.NextPageButton.Name = "NextPageButton";
+            this.NextPageButton.Size = new System.Drawing.Size(36, 36);
+            this.NextPageButton.TabIndex = 2;
+            this.NextPageButton.Text = "Next";
+            this.NextPageButton.UseVisualStyleBackColor = false;
+            this.NextPageButton.Click += new System.EventHandler(this.NextPageButton_Click);
+            // 
+            // PreviousPageButton
+            // 
+            this.PreviousPageButton.Location = new System.Drawing.Point(288, 0);
+            this.PreviousPageButton.Name = "PreviousPageButton";
+            this.PreviousPageButton.Size = new System.Drawing.Size(36, 36);
+            this.PreviousPageButton.TabIndex = 2;
+            this.PreviousPageButton.Text = "Prev";
+            this.PreviousPageButton.UseVisualStyleBackColor = false;
+            this.PreviousPageButton.Click += new System.EventHandler(this.PreviousPageButton_Click);
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.Location = new System.Drawing.Point(108, 0);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(36, 36);
+            this.ExitButton.TabIndex = 2;
+            this.ExitButton.Text = "Exit";
+            this.ExitButton.UseVisualStyleBackColor = false;
+            this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
             // PPTControlForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(360, 36);
+            this.Controls.Add(this.PreviousPageButton);
+            this.Controls.Add(this.NextPageButton);
+            this.Controls.Add(this.ExitButton);
             this.Controls.Add(this.SwitchToEraserButton);
             this.Controls.Add(this.SwitchToPenButton);
             this.Controls.Add(this.SwitchToPointerButton);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "PPTControlForm";
+            this.Opacity = 0.75D;
             this.Text = "PPT Control Form";
             this.TopMost = true;
             this.Load += new System.EventHandler(this.PPTControlForm_Load);
+            this.VisibleChanged += new System.EventHandler(this.PPTControlForm_VisibleChanged);
             this.ResumeLayout(false);
 
         }
@@ -90,5 +123,8 @@
         private System.Windows.Forms.Button SwitchToPointerButton;
         private System.Windows.Forms.Button SwitchToPenButton;
         private System.Windows.Forms.Button SwitchToEraserButton;
+        private System.Windows.Forms.Button NextPageButton;
+        private System.Windows.Forms.Button PreviousPageButton;
+        private System.Windows.Forms.Button ExitButton;
     }
 }
--- Element PPT Helper/Form1.cs
+++ Element PPT Helper/Form1.cs
@@ -7,39 +7,83 @@ namespace Element_PPT_Helper
 {
     public partial class PPTControlForm : Form
     {
-        PowerPoint.Application app = Globals.ThisAddIn.Application;
+        PowerPoint.SlideShowView pptView;
+
+        enum pointerTypes
+        {
+            pointer,
+            pen,
+            eraser,
+        }
 
         public PPTControlForm()
         {
             InitializeComponent();
         }
 
-        private void switchPointer(PowerPoint.PpSlideShowPointerType pointerType)
+        private void switchPointer(pointerTypes pointerType)
         {
-            app.ActivePresentation.SlideShowWindow.View.PointerType = pointerType;
+            PowerPoint.PpSlideShowPointerType newPointerType = PowerPoint.PpSlideShowPointerType.ppSlideShowPointerAutoArrow;
+            switch (pointerType)
+            {
+                case pointerTypes.pen:
+                    newPointerType = PowerPoint.PpSlideShowPointerType.ppSlideShowPointerPen;
+                    break;
+                case pointerTypes.eraser:
+                    newPointerType = PowerPoint.PpSlideShowPointerType.ppSlideShowPointerEraser;
+                    break;
+            }
+            pptView.PointerType = newPointerType;
         }
 
         private void PPTControlForm_Load(object sender, EventArgs e)
         {
+            pptView = Globals.ThisAddIn.Application.ActivePresentation.SlideShowWindow.View;
             Rectangle bounds = Screen.GetBounds(this);
             this.Top = bounds.Height - this.Height;
             this.Left = 0;
             this.Width = bounds.Width;
+            SwitchToPointerButton.Left = (this.Width - SwitchToPointerButton.Width - SwitchToPenButton.Width - SwitchToEraserButton.Width - ExitButton.Width) / 2;
+            SwitchToPenButton.Left = SwitchToPointerButton.Left + SwitchToPointerButton.Width;
+            SwitchToEraserButton.Left = SwitchToPenButton.Left + SwitchToPenButton.Width;
+            ExitButton.Left = SwitchToEraserButton.Left + SwitchToEraserButton.Width;
+            NextPageButton.Left = this.Width - NextPageButton.Width;
+            PreviousPageButton.Left = NextPageButton.Left - PreviousPageButton.Width;
         }
 
         private void SwitchToPointerButton_Click(object sender, EventArgs e)
         {
-            switchPointer(PowerPoint.PpSlideShowPointerType.ppSlideShowPointerAutoArrow);
+            switchPointer(pointerTypes.pointer);
         }
 
         private void SwitchToPenButton_Click(object sender, EventArgs e)
         {
-            switchPointer(PowerPoint.PpSlideShowPointerType.ppSlideShowPointerPen);
+            switchPointer(pointerTypes.pen);
         }
 
         private void SwitchToEraserButton_Click(object sender, EventArgs e)
         {
-            switchPointer(PowerPoint.PpSlideShowPointerType.ppSlideShowPointerEraser);
+            switchPointer(pointerTypes.eraser);
+        }
+
+        private void PreviousPageButton_Click(object sender, EventArgs e)
+        {
+            pptView.Previous();
+        }
+
+        private void NextPageButton_Click(object sender, EventArgs e)
+        {
+            pptView.Next();
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            pptView.Exit();
+        }
+
+        private void PPTControlForm_VisibleChanged(object sender, EventArgs e)
+        {
+            PPTControlForm_Load(sender, e);
         }
     }
 }
